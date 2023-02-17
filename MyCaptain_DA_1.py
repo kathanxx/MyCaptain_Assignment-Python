3e5b3986f58d6b87875e33e7388a459d70d77d40
kathanxx <105788412+kathanxx@users.noreply.github.com>
2023-02-17 22:01:58 +0530

    Add files via upload

---
+++ MyCaptain_DA_1.py
@@ -0,0 +1,8 @@
+set1={1,2,3,3} #set1
+set2={3,3,4,5,6,7} #set2
+print("SET 1 = ",set1) #display of set1
+print("SET 2 = ",set2) #display of set2
+print("Union : ",set1.union(set2)) #display of union of set1 and set2
+print("Intersection : ",set1.intersection(set2)) #display of intersection of set1 and set2
+print("Difference : ",set1.difference(set2)) #display of difference of set1 and set2
+print("Symmetric Difference : ",set1.symmetric_difference(set2)) #display of symmetric difference of set1 and set2
